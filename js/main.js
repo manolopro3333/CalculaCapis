a78390bf1b999edfc99251b69399e968bd95ff4b
__Aimbot__ <manuelvalencia1801@gmail.com>
2025-08-02 15:05:31 -0500

    capireference

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 notas50 = []
 notas20 = []
 autoevaluacion = 0.0
-coevaluacion = 0.0
 heteroevaluacion = 0.0
 semestral = 0.0
 
@@ -46,11 +45,7 @@ function enviar(nota,id) {
             break;
         case 2:
             autoevaluacion = nota
-        document.getElementById('mostrarauto').innerText = "\nTu AutoEvaluacion: " + autoevaluacion;
-            break;
-        case 4:
-            coevaluacion = nota
-            document.getElementById('mostrarco').innerText = "\nTu CoEvaluacion: " + coevaluacion;
+            document.getElementById('mostrarauto').innerText = "\nTu AutoEvaluacion: " + autoevaluacion;
             break;
         case 3:
             heteroevaluacion = nota
@@ -68,13 +63,11 @@ function reset() {
     notas50 = []
     notas20 = []
     autoevaluacion = 0.0
-    coevaluacion = 0.0
     heteroevaluacion = 0.0
     semestral = 0.0
     document.getElementById('mostrar50').innerText = "\ntu 50%: *Vacio*";
     document.getElementById('mostrar20').innerText = "\ntu 20%: *vacio*";
     document.getElementById('mostrarauto').innerText = "\nTu AutoEvaluacion: *vacio*";
-    document.getElementById('mostrarco').innerText = "\nTu CoEvaluacion: *vacio*";
     document.getElementById('mostrarhetero').innerText = "\nTu HeteroEvaluacion: *vacio*";
     document.getElementById('mostrarsemestral').innerText = "\nTu semestral: *vacio*";
 }
@@ -84,7 +77,7 @@ function reset() {
 function calcular() {
     sumado = 0
 
-    if ((notas50.length == 0) || (notas20.length == 0) || (autoevaluacion == 0.0) || (heteroevaluacion == 0.0) || (coevaluacion == 0.0)) {
+    if ((notas50.length == 0) || (notas20.length == 0) || (autoevaluacion == 0.0) || (heteroevaluacion == 0.0)) {
         errores(0)
         return 0
     }
@@ -94,25 +87,24 @@ function calcular() {
     for (i = 0; i < notas50.length; i++) {
         sumado += notas50[i]
     }
-    def50 = (sumado / notas50.length) * 0.5
+    def50 = (sumado / notas50.length) * 0.4
 
     sumado = 0
 
     for (i = 0; i < notas20.length; i++) {
         sumado += notas20[i]
     }
-    def20 = (sumado / notas20.length) * 0.2
+    def20 = (sumado / notas20.length) * 0.3
 
-    defauto = autoevaluacion * 0.025
-    defcoe = coevaluacion * 0.025
+    defauto = autoevaluacion * 0.05
     defhetero = heteroevaluacion * 0.05
     
-    defsin = def50 + def20 + defauto + defcoe + defhetero
+    defsin = def50 + def20 + defauto + defhetero
 
     if (semestral == 0) {
         document.getElementById('mostrar50').innerText = "\nTu definitiva sin contar la trimestral es de: " + defsin.toFixed(2);
         
-        necesario = 3 - defsin
+        necesario = 3.5 - defsin
 
         if (necesario > 1) {
             document.getElementById('mostrar20').innerText = "\n*PIERDES* LA MATERIA CON UN: " + (defsin + 1.2).toFixed(2) + " (eso si SACAS un 5.0 en tu SEMESTRAL)";
@@ -120,7 +112,7 @@ function calcular() {
             document.getElementById('mostrar20').innerText = "\nAunque usted saque 1.0 igual pasa.";
         }else {
             nesemestral = (necesario*100) / 20
-            document.getElementById('mostrar20').innerText = "\nPara llegar a 3.0 necesitas sacar en la semestral: " + nesemestral.toFixed(2);
+            document.getElementById('mostrar20').innerText = "\nPara llegar a 3.5 necesitas sacar en la semestral: " + nesemestral.toFixed(2);
         }
         
         document.getElementById('mostrarauto').innerText = "";
@@ -143,7 +135,7 @@ function calcular() {
 
 function errores(id) {
     if (id == 0) {
-        alert("No se ha rellenado campos en el 50% o 20%")
+        alert("No se ha rellenado campos en el 40% o 30% o auto o hetero")
     }
     if (id == 1) {
         alert("El numero debe de estar en un rango de 1 a 5.")
